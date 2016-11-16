9d17e0ccab9476282fddec1e6938c45e22adf17c
Colin Rotherham <work@colinr.com>
2016-11-16 10:24:22 +0000

    Use string literals for redirects

--- app/routes.js
+++ app/routes.js
@@ -60,7 +60,7 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
   // No partner or not asked yet
   if (typeof answers.claimant.partner === 'undefined') {
-    res.redirect('/' + type + '/questions/partner?claimant');
+    res.redirect(`/${type}/questions/partner?claimant`);
   }
 
   // Override this outcome based on partner
@@ -71,13 +71,13 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
       // Claimant is EEA, doesn't work
       if (answers.claimant.isEEA && answers.claimant.employeeStatus && answers.claimant.employeeStatus.dontWork === 'true') {
-        res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+        res.redirect(`/${type}/outcomes/END003?${claimantType}`);
         return;
       }
 
       // Claimant is Non-EEA, recourse to public funds
       if (!answers.claimant.isEEA && answers.claimant.noRecourseToPublicFunds === 'yes') {
-        res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+        res.redirect(`/${type}/outcomes/END003?${claimantType}`);
         return;
       }
     }
@@ -87,7 +87,7 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
       // Claimant is a refugee
       if (answers.claimant.refugee === 'yes') {
-        res.redirect('/' + type + '/outcomes/END008?' + claimantType);
+        res.redirect(`/${type}/outcomes/END008?${claimantType}`);
         return;
       }
     }
@@ -97,7 +97,7 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
       // Claimant is EEA, in work
       if (answers.claimant.isEEA && answers.claimant.employeeStatus && answers.claimant.employeeStatus.employed === 'true') {
-        res.redirect('/' + type + '/outcomes/END002?' + claimantType);
+        res.redirect(`/${type}/outcomes/END002?${claimantType}`);
         return;
       }
     }
@@ -107,7 +107,7 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
       // Claimant is Non-EEA, no recourse to public funds
       if (!answers.claimant.isEEA && answers.claimant.noRecourseToPublicFunds === 'no') {
-        res.redirect('/' + type + '/outcomes/END009?' + claimantType);
+        res.redirect(`/${type}/outcomes/END009?${claimantType}`);
         return;
       }
     }
@@ -117,7 +117,7 @@ router.all('/:type/outcomes/:outcomeId', function (req, res, next) {
 
       // Claimant is a UK national
       if (answers.claimant.ukNational === 'yes') {
-        res.redirect('/' + type + '/outcomes/END001?' + claimantType);
+        res.redirect(`/${type}/outcomes/END001?${claimantType}`);
         return;
       }
     }
@@ -139,16 +139,16 @@ router.all('/:type/questions/uk-national', function (req, res, next) {
     // UK national
     if (ukNational == 'yes') {
       answers[claimantType].isEEA = true;
-      res.redirect('/' + type + '/outcomes/END001?' + claimantType);
+      res.redirect(`/${type}/outcomes/END001?${claimantType}`);
     }
 
     // Non-UK national
     else if (ukNational == 'no') {
-      res.redirect('/' + type + '/questions/nationality?' + claimantType);
+      res.redirect(`/${type}/questions/nationality?${claimantType}`);
     }
 
     else if (res.locals.isPartnerFlow && ukNational === 'unknown') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
@@ -174,17 +174,17 @@ router.all('/:type/questions/nationality', function (req, res, next) {
 
       // Croatia straight to outcome
       if (nationality === 'Croatia') {
-        res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+        res.redirect(`/${type}/outcomes/END003?${claimantType}`);
       }
 
       // Continue
-      res.redirect('/' + type + '/questions/employee-status?' + claimantType);
+      res.redirect(`/${type}/questions/employee-status?${claimantType}`);
     }
 
     // Non-EEA nationality
     else if (listNonEEA.indexOf(nationality) !== -1) {
       answers[claimantType].isEEA = false;
-      res.redirect('/' + type + '/questions/refugee?' + claimantType);
+      res.redirect(`/${type}/questions/refugee?${claimantType}`);
     }
   }
 
@@ -202,17 +202,17 @@ router.all('/:type/questions/employee-status', function (req, res, next) {
 
     // Self-employed
     if (employeeStatus.selfEmployed === 'true') {
-      res.redirect('/' + type + '/questions/self-employed-proof?' + claimantType);
+      res.redirect(`/${type}/questions/self-employed-proof?${claimantType}`);
     }
 
     // Employed
     else if (employeeStatus.employed === 'true') {
-      res.redirect('/' + type + '/outcomes/END002?' + claimantType);
+      res.redirect(`/${type}/outcomes/END002?${claimantType}`);
     }
 
     // Not working
     else if (employeeStatus.dontWork === 'true') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
@@ -230,12 +230,12 @@ router.all('/:type/questions/self-employed-proof', function (req, res, next) {
 
     // Self-employed proof can be provided
     if (selfEmployedProof === 'yes') {
-      res.redirect('/' + type + '/outcomes/END006?' + claimantType);
+      res.redirect(`/${type}/outcomes/END006?${claimantType}`);
     }
 
     // Self-employed proof can't be provided
     else if (selfEmployedProof === 'no' || res.locals.isPartnerFlow && selfEmployedProof === 'unknown') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
@@ -253,16 +253,16 @@ router.all('/:type/questions/refugee', function (req, res, next) {
 
     // Refugee
     if (refugee === 'yes') {
-      res.redirect('/' + type + '/outcomes/END008?' + claimantType);
+      res.redirect(`/${type}/outcomes/END008?${claimantType}`);
     }
 
     // Non-refugee
     else if (refugee === 'no') {
-      res.redirect('/' + type + '/questions/no-recourse-to-public-funds?' + claimantType);
+      res.redirect(`/${type}/questions/no-recourse-to-public-funds?${claimantType}`);
     }
 
     else if (res.locals.isPartnerFlow && refugee === 'unknown') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
@@ -283,17 +283,17 @@ router.all('/:type/questions/partner', function (req, res, next) {
 
       // Claimant was actually END003 before partner flow
       if (outcomeId === 'END003') {
-        res.redirect('/' + type + '/outcomes/END003?partner');
+        res.redirect(`/${type}/outcomes/END003?${claimantType}`);
       }
 
       // Assume still qualifying
       else {
-        res.redirect('/' + type + '/questions/uk-national?partner');
+        res.redirect(`/${type}/questions/uk-national?partner`);
       }
     }
 
     else if (partner === 'no' && outcomeId) {
-      res.redirect('/' + type + '/outcomes/' + outcomeId + '?claimant');
+      res.redirect(`/${type}/outcomes/${outcomeId}?claimant`);
     }
   }
 
@@ -311,16 +311,16 @@ router.all('/:type/questions/no-recourse-to-public-funds', function (req, res, n
 
     // Stamped visa
     if (noRecourseToPublicFunds === 'yes') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
 
     // No stamped visa
     else if (noRecourseToPublicFunds === 'no') {
-      res.redirect('/' + type + '/questions/out-of-uk?' + claimantType);
+      res.redirect(`/${type}/questions/out-of-uk?${claimantType}`);
     }
 
     else if (res.locals.isPartnerFlow && noRecourseToPublicFunds === 'unknown') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
@@ -338,16 +338,16 @@ router.all('/:type/questions/out-of-uk', function (req, res, next) {
 
     // Out of UK more than 4 weeks
     if (outOfUk === 'yes') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
 
     // Out of UK less than 4 weeks
     else if (outOfUk === 'no') {
-      res.redirect('/' + type + '/outcomes/END009?' + claimantType);
+      res.redirect(`/${type}/outcomes/END009?${claimantType}`);
     }
 
     else if (res.locals.isPartnerFlow && noRecourseToPublicFunds === 'unknown') {
-      res.redirect('/' + type + '/outcomes/END003?' + claimantType);
+      res.redirect(`/${type}/outcomes/END003?${claimantType}`);
     }
   }
 
