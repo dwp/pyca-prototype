6f6ab35473d4520d29bab08e80724dd771120102
Paul Smith <mail@airfront.co.uk>
2017-03-08 09:17:30 +0000

    separates the live version in the list

--- app/subapps.js
+++ app/subapps.js
@@ -18,25 +18,25 @@ let subApps = new Array()
  * @return {object}            data derrived from the routes file and it's path
  */
 let getSubAppData = function(currentPath) {
-  
+
 	let splitPathArray = currentPath.split('/')
-	
-	// this takes the file path and returns the relative path to it 
-	// within the prototyping kit 
-  let computedPath = _.join( 
-			_.slice( 
-				splitPathArray, ( 
-					_.indexOf(splitPathArray, 'views') +1 
-				) 
+
+	// this takes the file path and returns the relative path to it
+	// within the prototyping kit
+  let computedPath = _.join(
+			_.slice(
+				splitPathArray, (
+					_.indexOf(splitPathArray, 'views') +1
+				)
 			),
-		'/' 
+		'/'
 	);
-	
+
 	// gets the sub directory name
 	let appDirName = computedPath.split('/')[1]
-  
+
   let appRelativePath = `${appsDir}/${appDirName}`
-  
+
 	// the 'absolute' path of the app e.g '/apps/version-1/'
 	let appAbsolutePath = `/${appsDir}/${appDirName}`
 
@@ -45,21 +45,21 @@ let getSubAppData = function(currentPath) {
 	// the title based on the app's directory/folder name. Set to be sentance-case
 	let title = sentenceCase(appDirName)
 	let titleSlug = title.replace(/\s+/g, '-').toLowerCase()
-	
+
 	// returns and object of data derrived from the subapp path that was passed in
   return {
-		
+
 		// a formatted string for the title of the subapp
 		title: title,
-		
+
 		// the directory name for the subapp
     appDirName: appDirName,
-		
+
 		// a string used to give the app a unique body class
 		body_class: title.replace(/\s+/g, '-').toLowerCase(),
 		slug: titleSlug,
     startURL: `${appAbsolutePath}/views/`,
-		
+
 		// url paths constructed from the passed in subapp path
 		urlPaths: {
 			appRoot: appAbsolutePath,
@@ -69,9 +69,9 @@ let getSubAppData = function(currentPath) {
 			stylesPath: `${appAbsolutePath}/assets/sass/`,
 			imagesPath: `${appAbsolutePath}/assets/images/`
 		},
-		
+
 		// file paths, to be used primarily by nunjucks for rendering layouts,
-		// accessing routes, etc. 
+		// accessing routes, etc.
 		filePaths: {
 			routesFile: computedPath,
 			subAppDir: path.dirname(computedPath),
@@ -81,21 +81,21 @@ let getSubAppData = function(currentPath) {
 			configFile: `${path.dirname(computedPath)}/config.js`,
 			dataFile: `${path.dirname(computedPath)}/data.js`
 		},
-		
+
 		// route strings
 		route: {
 			root: appRouteString,
 			rootRel: appRouteString.substr(1),
 			page: appRouteString + '/:page'
 		}
-		
-		
+
+
   }
-	
+
 }
 
 /**
- * this route is a fudge to be able to serve the assets without editing the 
+ * this route is a fudge to be able to serve the assets without editing the
  * gov.uk prototying kit's server.js file to specifically serve the usuaul way
  *  using static middleware.
  */
@@ -114,14 +114,14 @@ router.get('/apps/:subapp*/assets/:type/:file*', function(req, res){
  * loop over the sub 'routes' files and add them to the overall router
  */
 glob.sync(baseSubAppPath + appsDir + '/**/*-routes.js').forEach(function(currentPath, index){
-  
+
 	// get some data based on the current subapp path
 	let appData = getSubAppData(currentPath)
-	
+
 	// add specific subApp config (can override some of app/config)
 	appData.config = require(`${baseSubAppPath + appData.filePaths.configFile}`)
 	appData.data = require(`${baseSubAppPath + appData.filePaths.dataFile}`)
-	
+
 	// push that to a collection of all the subapps
 	subApps.push(appData)
 
@@ -131,9 +131,9 @@ glob.sync(baseSubAppPath + appsDir + '/**/*-routes.js').forEach(function(current
     `${appData.urlPaths.appRoot}/views/:page*`,
     `${appData.urlPaths.appRoot}/views/**/:page*`
   ]
-	
+
 	// grabs the subnavigation from data.js for a subsection if the key is found
-	// within subnavs object. Sets it within the 'currentApp' object eventually 
+	// within subnavs object. Sets it within the 'currentApp' object eventually
 	// passed to the context
 	router.all(`${appData.urlPaths.appRoot}/views/:subsection/*`, (req,res,next) => {
 		let thisSubSection = req.params.subsection
@@ -144,9 +144,9 @@ glob.sync(baseSubAppPath + appsDir + '/**/*-routes.js').forEach(function(current
 		}
 		next()
 	})
-		
+
 	router.all(subRoutes, function(req,res,next){
-		
+
 	  if (!req.session[appData.slug]){
 	    req.session[appData.slug] = {};
 	  }
@@ -157,34 +157,35 @@ glob.sync(baseSubAppPath + appsDir + '/**/*-routes.js').forEach(function(current
 	      req.session[appData.slug][i] = req.body[i];
 	    }
 	  }
-    
+
     appData.session = req.session[appData.slug]
-		
+
 	  _.merge(res.locals, {
       session: req.session,
 	    currentApp: appData,
 			postData: (req.body ? req.body : false)
 	  }, appData.config.overrides)
-	  
+
 		next();
-    
+
 	})
 
 	// require the current subapp's routes file passing the overall app router
 	// and the subapp's data object
 	require(currentPath)(router, appData)
-	
+
 	// if the user hits the root of the subapp's views, then redirect to the
 	// index page
 	router.all(appData.route.root, function(req,res,next){
 		return res.redirect('index')
 	});
-	
+
 });
 
 // Route index page including the collection of apps in the context
 router.get('/', function (req, res, next) {
-  res.locals.apps = subApps
+  // remove 'live version from the list' we will link to that directly.
+  res.locals.apps = subApps.filter(item => item.slug !== 'live');
   next()
 })
 
