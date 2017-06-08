1f09a6d3f8890996d9aae116dfeb10ca31e8558b
Paul Smith <mail@airfront.co.uk>
2017-06-08 14:11:09 +0100

    adds new cors header

--- app/routes.js
+++ app/routes.js
@@ -25,6 +25,7 @@ router.use('*', (req,res,next) => {
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
