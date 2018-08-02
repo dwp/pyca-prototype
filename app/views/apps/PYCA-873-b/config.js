7bda055f213c62efdd8ad69afd8489e9c2a390cd
Ash-Wilson <ashleighwilson.digitaldwp@gmail.com>
2018-08-02 12:16:10 +0100

    Copied quick win branch for other testing

---
+++ app/views/apps/PYCA-873-b/config.js
@@ -0,0 +1,27 @@
+// Use this file to change subapp configuration.
+module.exports = {
+	title: 'PYCA-873/859-b (split out employment question)',
+
+	// properties in here will override those in that affect the templates
+	// with in the global config file (app/config.js)
+	overrides: {
+
+	  // Service name used in header. Eg: 'Renew your passport'
+	  // serviceName: 'Renew your passport'
+
+	  // Cookie warning - update link to service's cookie page.
+	  // cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
+
+	},
+
+	// changes the default css file name for the this sub-application
+	// you will need to rename the file as well.
+	// file is located: app/views/apps/[SUB APPLICATION DIR]/assets/sass/
+	cssFile: 'sub-application.css',
+
+	// changes the default javascript file name for the this sub-application
+	// sub-application but you will need to rename the file as well.
+	// file is located: app/views/apps/[SUB APPLICATION DIR]/assets/javascripts/
+	jsFile: 'sub-application.js'
+
+}
