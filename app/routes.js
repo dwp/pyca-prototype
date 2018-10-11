cf6050f69a98c6b1b5d725ab9408ed64c5538edc
Colin Rotherham <work@colinr.com>
2018-10-11 14:03:53 +0100

    Add country picker

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,10 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Data sources
+router.all('/data/:data/source/:source', (req, res) => {
+  const { data, source } = req.params
+  res.json(require(`./data/${data}/source/${source}`))
+})
+
 module.exports = router
