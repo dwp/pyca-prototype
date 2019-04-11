f1f424e749d72a404c1d91f533d09fc4cc517ea2
Colin Rotherham <work@colinr.com>
2019-04-11 11:40:12 +0100

    Update prototype version

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Prove you can apply',
 
   // Latest release number
-  productName: 'v8.1.1',
+  productName: 'v9.0.0',
 
   // Default port that prototype runs on
   port: '3000',
