52af245ad48371a9befcd915d172fa6f4292c2eb
Fey Ijaware <fshola4real@gmail.com>
2020-01-15 10:44:21 +0000

    Update config.js

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Prove you can apply',
 
   // Latest release number
-  productName: 'v22.1.0',
+  productName: 'v23.0.0',
 
   // Default port that prototype runs on
   port: '3000',
