977acaadb51b5acecc1b517203c310695d74bf25
Fey Ijaware <fshola4real@gmail.com>
2020-05-14 15:44:00 +0100

    bumped up version number to 29

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Prove you can apply',
 
   // Latest release number
-  productName: 'v28.0.0',
+  productName: 'v29.0.0',
 
   // Default port that prototype runs on
   port: '3000',
