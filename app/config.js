54bbd2db9f20dce864d653fd3a870e7139131f9e
Neil Mills <mail@neil-mills.co.uk>
2021-03-12 13:11:48 +0000

    Release 36.0.0 updates

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ module.exports = {
   serviceName: 'Prove you can apply for benefits in the UK',
 
   // Latest release number
-  productName: 'V35.0.0',
+  productName: 'V36.0.0',
 
   // Default port that prototype runs on
   port: '3000',
